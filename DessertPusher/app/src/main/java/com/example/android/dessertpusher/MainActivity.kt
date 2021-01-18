efcd26ad0c94770cacf5fec6dc61b1f209867b36
Otto <ottofernandes2012@gmail.com>
2021-01-18 20:39:16 -0300

    Dessert Pusher finished, but README not added

--- DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.dessertpusher
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -29,10 +28,15 @@ import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "key_revenue"
+const val KEY_DESSERTS_SOLD = "key_desserts_sold"
+const val KEY_DESSERT_TIMER_TIME = "key_dessert_timer_time"
+
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -76,6 +80,13 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         onDessertClicked()
       }
 
+      dessertTimer = DessertTimer(this.lifecycle)
+
+      if(savedInstanceState != null) {
+        revenue = savedInstanceState.getInt(KEY_REVENUE)
+        dessertsSold = savedInstanceState.getInt(KEY_DESSERTS_SOLD)
+        dessertTimer.secondsCount = savedInstanceState.getInt(KEY_DESSERT_TIMER_TIME)
+      }
       // Set the TextViews to the right values
       binding.revenue = revenue
       binding.amountSold = dessertsSold
@@ -152,8 +163,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     }
 
     override fun onStart() {
-        super.onStart()
-        Timber.i("onStart called")
+      super.onStart()
+      Timber.i("onStart called")
     }
 
   override fun onResume() {
@@ -180,4 +191,13 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     super.onStop()
     Timber.i("onStop called")
   }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putInt(KEY_REVENUE, revenue)
+    outState.putInt(KEY_DESSERTS_SOLD, dessertsSold)
+    outState.putInt(KEY_DESSERT_TIMER_TIME, dessertTimer.secondsCount)
+    Timber.i("onSaveInstanceState called")
+  }
+
 }
