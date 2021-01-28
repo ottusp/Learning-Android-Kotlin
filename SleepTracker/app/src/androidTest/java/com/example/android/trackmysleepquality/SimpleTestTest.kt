01af85385a2b9310fc99dd2a6f882e4e364f7a23
Otto <ottofernandes2012@gmail.com>
2021-01-27 21:04:51 -0300

    SleepTracker app created, but not working yet. After passing a lot of time trying to figure out how to run Instrumentation tests on device, I finally got it

---
+++ SleepTracker/app/src/androidTest/java/com/example/android/trackmysleepquality/SimpleTestTest.kt
@@ -0,0 +1,12 @@
+package com.example.android.trackmysleepquality
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SimpleTestTest {
+    
+    @Test
+    fun simpliestTest() {
+        assertEquals(2 + 2, 4)
+    }
+}
